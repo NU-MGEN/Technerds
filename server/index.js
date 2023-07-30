b14701b9e2d5208aed4e75f0b543e7fcda2e1590
sstej88 <saitejsunkara9@gmail.com>
2023-07-30 09:39:55 -0400

    formatted file

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const cors = require('cors');
+/** @format */
 
-const openAiRoutes = require('./routes/openAI');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const app = express()
-const port = 7912
+const openAiRoutes = require("./routes/openAI");
+
+const app = express();
+const port = 7912;
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use("/openAI", openAiRoutes);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
