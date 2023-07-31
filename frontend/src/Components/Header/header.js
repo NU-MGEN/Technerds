67f4beff5b94a629f6ab06262693eb68a1da8a4a
sstej88 <saitejsunkara9@gmail.com>
2023-07-30 21:45:29 -0400

    Header Updated

--- frontend/src/Components/Header/header.js
+++ frontend/src/Components/Header/header.js
@@ -1,13 +1,11 @@
 /** @format */
 
 import React from "react";
-import logo  from "../../assets/OmniBotRoundedCorners.png";
+import logo from "../../assets/OmniBotRoundedCorners.png";
 
 const Header = () => {
   return (
-    <nav
-      className="navbar navbar-dark-work"
-      aria-label="omni-bot-navbar">
+    <nav className="navbar navbar-dark-work" aria-label="omni-bot-navbar">
       <div className="container-fluid">
         <div id="omni-bot-navbar">
           <ul className="navbar-nav justify-content-center">
@@ -19,7 +17,7 @@ const Header = () => {
                   height="38"
                   className="me-3"
                   alt="Omni-Bot Logo"></img>
-                Welcome to the Best Learning Experience Ever with OmniBot.
+                Welcome to the Best Learning Experience Ever with Technerds.
               </span>
             </li>
           </ul>
